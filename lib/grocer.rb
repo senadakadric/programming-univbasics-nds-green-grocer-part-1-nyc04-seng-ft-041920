e5cea6c38011e7d5c99e57179e4348df7187fd53
Senada Kadric <skadric@fordham.edu>
2020-05-04 21:24:04 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,8 +1,14 @@
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
-  #
-  # Consult README for inputs and outputs
-
+  
+  #iterate through each array element of the collection
+  #in this case, the element will be a hash of the items and their attributes
+  collection.each do |hash|
+    if hash[:item] == name
+      return hash
+    end
+  end
+return nil
 end
 
 def consolidate_cart(cart)
